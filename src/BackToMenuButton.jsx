fa49177ee30bbbc2e257f4b749bea1d6ceabbd46
Nicholas Gustafson <nicholasgustafson@Nickbook-2.local>
2023-10-27 21:55:12 -0400

    fixing back to menu button reset

--- src/BackToMenuButton.jsx
+++ src/BackToMenuButton.jsx
@@ -1,15 +1,25 @@
 // src/components/BackToMenuButton.jsx
 
 import React from 'react';
-import { useDispatch } from 'react-redux';
-import { resetGameMode } from './redux/gameSlice';
+import { useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setGameMode, setTiles,setFlippedIndices, setShowUnmatch, setIsWon, setIsShuffling, setStartTime, setElapsedTime, selectIntervalId } from './redux/gameSlice';
 
 function BackToMenuButton() {
   const dispatch = useDispatch();
+  const intervalId = useSelector(selectIntervalId);
 
-  const handleBackToMenu = () => {
-    dispatch(resetGameMode());
-  };
+  const handleBackToMenu = useCallback(() => {
+    dispatch(setGameMode(null));
+    dispatch(setTiles([]));
+    dispatch(setFlippedIndices([]));
+    dispatch(setShowUnmatch(false));
+    dispatch(setIsWon(false));
+    dispatch(setIsShuffling(false));
+    dispatch(setStartTime(null));
+    dispatch(setElapsedTime(0));
+    clearInterval(intervalId);  // Ensure the timer is stopped
+  }, [dispatch, intervalId]);
 
   return (
     <button className="menu-button" onClick={handleBackToMenu}>
