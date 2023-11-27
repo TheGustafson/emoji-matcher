463808d4626ab81b478a343857b021cd7a2ef972
Nicholas Gustafson <nicholasgustafson@Nickbook-2.local>
2023-11-26 21:15:39 -0500

    no padding on icons

--- sw.js
+++ sw.js
@@ -1,20 +1,18 @@
-// This is the "Offline copy of pages" service worker
+const CACHE_NAME = "cool-cache";
 
-const CACHE = "pwabuilder-offline";
+// Add whichever assets you want to precache here:
+const PRECACHE_ASSETS = ["/src/", "/icons/"];
 
-importScripts(
-  "https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js"
-);
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+// Listener for the install event - precaches our assets list on service worker install.
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(PRECACHE_ASSETS);
+    })()
+  );
 });
 
-workbox.routing.registerRoute(
-  new RegExp("/*"),
-  new workbox.strategies.StaleWhileRevalidate({
-    cacheName: CACHE,
-  })
-);
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
+});
