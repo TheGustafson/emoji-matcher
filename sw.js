ea4b68837ae484213137ff11dc419940285d0439
Nicholas Gustafson <nicholasgustafson@Nickbook-2.local>
2023-11-26 22:44:52 -0500

    service worker testing

--- sw.js
+++ sw.js
@@ -1,18 +1,37 @@
-const CACHE_NAME = "cool-cache";
+const CACHE_NAME = "matchmoji-cache-00006";
 
-// Add whichever assets you want to precache here:
+// Update this list with the paths of assets you want to cache
 const PRECACHE_ASSETS = ["/src/", "/icons/"];
 
-// Listener for the install event - precaches our assets list on service worker install.
+// Install event - caches assets and forces new service worker to take control immediately
 self.addEventListener("install", (event) => {
   event.waitUntil(
     (async () => {
       const cache = await caches.open(CACHE_NAME);
-      cache.addAll(PRECACHE_ASSETS);
+      await cache.addAll(PRECACHE_ASSETS);
+      self.skipWaiting(); // Forces this version of the service worker to activate immediately
     })()
   );
 });
 
+// Activate event - claims clients and cleans up old caches
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    (async () => {
+      // Claim clients immediately
+      self.clients.claim();
+
+      // Get all the cache keys (cache names)
+      const cacheKeys = await caches.keys();
+
+      // Wait for all old caches to be deleted
+      await Promise.all(
+        cacheKeys.map((cacheKey) => {
+          if (cacheKey !== CACHE_NAME) {
+            return caches.delete(cacheKey); // Delete caches not matching the current CACHE_NAME
+          }
+        })
+      );
+    })()
+  );
 });
